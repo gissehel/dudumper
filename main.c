582b3432c58910dc19d56b0bede0109abe90c5ea
gissehel <public-devgit-dantus@gissehel.org>
2018-01-07 18:09:37 +0100

    * append right extension

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
-#define _XOPEN_SOURCE 500
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "node_info.h"
 #include "node_parser.h"
@@ -11,12 +10,20 @@
 #include "string_dumper_file.h"
 #include "string_dumper_stdout.h"
 
+char* append_extension(const char* base_filename, const char* extension) {
+    int bufferlen = strlen(base_filename)+strlen(extension)+1+1;
+    char* buffer = malloc(sizeof(char)*bufferlen);
+    snprintf(buffer, bufferlen, "%s.%s", base_filename, extension);
+    return buffer;
+}
+
 int main(int argc, char** argv) {
     char* path;
     struct string_dumper* string_dumper = NULL;
     struct node_parser* node_parser = NULL;
 
     char* output_filename = "z-output";
+    char* buffer_output_filename = NULL;
     bool use_json = true;
     bool use_sqlite = true;
 
@@ -26,11 +33,24 @@ int main(int argc, char** argv) {
         path = argv[1];
     }
 
-    if (output_filename == NULL) {
+    if (output_filename != NULL) {
+        if (use_json) {
+            buffer_output_filename = append_extension(output_filename, "json");
+        } else if (use_sqlite) {
+            buffer_output_filename = append_extension(output_filename, "sql");
+        }
+    }
+
+    if (buffer_output_filename == NULL) {
         string_dumper = string_dumper_stdout_create();
     } else {
-        string_dumper = string_dumper_file_create(output_filename);
+        string_dumper = string_dumper_file_create(buffer_output_filename);
     }
+
+    if (buffer_output_filename != NULL) {
+        free(buffer_output_filename);
+    }
+
 
     if (use_json) {
         node_parser = node_parser_json_create( string_dumper );
